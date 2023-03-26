0161ae18dd4c352319a85023c1ab2e95dc15065e
advakhov97@mail.ru <advakhov97@mail.ru>
2023-03-26 17:43:51 +0300

    Другой метод указания обязательности заполения полей [Required] перед каждым объектом класса Restaurant, который требует заполнение.

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace OdeToFood.Data.Models
     public class Restaurant
     {
         public int ID { get; set; }
+
+        [Required]
         public string StarMishlen { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Type of food")]
         public CuisineType Cuisine { get; set; }
     }
 }
